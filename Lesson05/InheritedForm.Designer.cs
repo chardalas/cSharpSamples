d32b34d7c31327a79ad3b7c36093b9f8ead8af2c
Chardalas <emmhard@gmail.com>
2017-05-16 13:33:10 +0200

    This is made from my cmd

---
+++ Lesson05/InheritedForm.Designer.cs
@@ -0,0 +1,52 @@
+﻿namespace Lesson05
+{
+	partial class InheritedForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.BackColor = System.Drawing.SystemColors.Control;
+			// 
+			// InheritedForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.BackColor = System.Drawing.Color.DarkSalmon;
+			this.ClientSize = new System.Drawing.Size(291, 164);
+			this.Name = "InheritedForm";
+			this.Text = "Inhereted Form";
+			this.Load += new System.EventHandler(this.InheritedForm_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+	}
+}
