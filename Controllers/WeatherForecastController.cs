fa7009e16830ce7bd706150d978b77ecd4e0d60b
Chardalas Emmanouil <emmhard@gmail.com>
2021-09-19 17:15:55 +0200

    Update WeatherForecastController.cs
    Add one more mood

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace DependencyInjectionInAction.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Wet"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
